f41de176c95171c637b7932e573da928f3823343
ArsenPetrosianAPK <arsen.apk@mail.ru>
2020-02-29 20:55:27 +0400

    Lesson6github

---
+++ Lesson6GitGithub.py
@@ -0,0 +1,15 @@
+# Creat a new repository
+
+# clone url 
+
+# git clone url 
+
+# git status = information about situation
+
+# git add file, git add -A = add files
+
+# git commit -m 'add python' = comment
+
+# git push
+
+# git pull = install file
