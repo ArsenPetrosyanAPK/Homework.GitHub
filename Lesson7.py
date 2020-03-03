6ed61da01ab036429c06770f91a32f052c55d1ff
ArsenPetrosianAPK <arsen.apk@mail.ru>
2020-03-03 21:14:18 +0400

    lessont7

---
+++ Lesson7.py
@@ -0,0 +1,81 @@
+#a = input('Please enter firt number: ')
+#b = input('(+), (-), (*), (/): ')
+#c = input('Please enter second number')
+
+#if b == ('+'):
+#	print(int(a) + int(c)) 
+
+#if b == ('-'):
+#	print(int(a) - int(c))
+
+#if b == ('*'):
+#	print(int(a) * int(c))
+
+#if b == ('/'):
+#	print(int(a) / int(c))
+
+#import sys
+#x = (5)
+#print(sys.getsizeof(x))
+
+#a = 10
+#b = 10
+#if b > a:
+#	print('b is big')
+#elif a > b:
+#	print('a is big')
+#else:
+#	print('a iss big')
+
+#country = input('Which is the biggest peoples country in the world? ')
+#if country == 'China' or country == 'china':
+#	print('you are right')
+#else:
+#	print('its a wrong answer ')
+
+#import random
+
+#zar_1 = random.randint(1,6)
+#zar_2 = random.randint(1,6)
+#my_zar_1 = random.randint(1,6)
+#my_zar_2 = random.randint(1,6)
+#myresult = zar_1 + zar_2
+#compresult = my_zar_1 + my_zar_2
+#if myresult > compresult:
+#	print('You win')
+#elif myresult < compresult:
+#	print("Computer Win")
+#else:
+#	 myresult ==  compresult
+#	 print("no one is win")
+
+#year = int(input("please enter a year"))
+
+#if year % 400 == 0:
+#	print('February', year, 'has 29 days, .')
+#elif year % 100 == 0 and year % 400 == 0:
+#	print('February', year, 'has 29 days.')
+#else:
+#    print('February', year, 'has 28 days.')
+
+#sxal 	
+
+
+#god = int(input('Tari'))
+#if god % 4 != 0 or god % 100 == 0 and god % 400 != 0:
+#	print(god,"nahanj tari chi")
+#else:
+#	print(god, ' nahanj tari e')
+
+
+
+
+#inputov kam random qani tarekan 70 ic mec te poqr
+
+#import random as ran
+#x = ran.randint(1,50)
+#y = int(input('How old are you? '))
+#if y > 18 and y <= 25:
+#	print('you are enjoyed ')
+#else:
+#	print('You are not accept ')
