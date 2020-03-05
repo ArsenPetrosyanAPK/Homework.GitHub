39146c013f85b7dddfbc0728a6875db534121bc1
ArsenPetrosianAPK <arsen.apk@mail.ru>
2020-03-05 21:04:42 +0400

    Lesson8

---
+++ Lesson8.whilefor.py
@@ -0,0 +1,61 @@
+#import calendar
+#y = int(input("Input the year: "))
+#m = int(input("Input the mounth: "))
+#print(calendar.month(y, m))
+
+
+
+#i = int(input('year'))
+#if i > 1582:
+#	if i % 4 != 0:
+#		print("no")
+#	elif i % 100 == 0 and i % 400 == 0:
+#		else:
+#		print('no')
+#	else:
+#	print('yes')
+
+
+#name = 'aram artur armen vaxo gago vrdo'
+#i = input('name: ')
+#if i in name:
+#	print("yes")
+#else:
+#	print('no')
+
+
+
+#age = int(input('please enter age: '))
+#if age <= 2:
+#	print(age * 10.5)
+#else:
+#	print((age - 2) * 4 + 2 )
+
+
+#c = int(input('please enter celsius: '))
+#if c == 0:
+#	print('f = 32: ')
+#else:
+#	print(32 + 1.8 * c)
+
+#import time
+
+#f = float(input("Farenheit = "))
+#if f == 32:
+#	print("0 c")
+#else:
+#	c = f - 32
+#	time.sleep(1)
+#	print(c / 1.8)
+
+
+# raund = Kloracnum e Tivy
+
+
+#email = input('please enter email: ')
+#if '@' not in email:
+#	print('wrong')
+#else:
+#	print('right')
+
+#year name usurname mail password
