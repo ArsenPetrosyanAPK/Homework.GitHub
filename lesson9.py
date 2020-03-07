8dd2dab8968b874d33bac2918bee344fa96fe5f8
ArsenPetrosianAPK <arsen.apk@mail.ru>
2020-03-07 20:56:31 +0400

    lesson9

---
+++ lesson9.py
@@ -0,0 +1,73 @@
+# LOOP
+
+#names = 'John'
+#for name in names:
+#	print(name)
+
+#print(names[1])
+
+
+#range() asum e ciklin qani angam gorcoxutyuny katari
+
+#for i in range(5):
+#	print('Ani')
+
+
+#x = 0
+#for i in range(5):
+#	x += 1
+#	if x != 4:
+#		print('right')
+#	else:
+#		print("wrong")
+
+
+#for x in range(2, 11):
+#	print(x)
+
+# Nested Loops
+
+#name = 'Arsen'
+#lastname = 'Petrosian'
+#for x  in name:
+#	for y in lastname:
+#		print(x,y)
+
+# While
+# anverjanali cikl
+
+
+#i = 2
+#while i < 6:
+#	print(i)
+#	i += 1
+
+
+#i = 1
+#while i < 6:
+#	print(i)
+#	if i == 3:
+#		break
+#	i += 1
+
+# Z  A  R
+
+
+#import random
+#comp = random.randrange(1, 7)
+#while True:
+#	user = random.randrange(1, 7)
+#	if user > comp:
+#		print('comp', comp, 'user', user, 'user is win')
+#		break
+#	elif comp == 6 and user == 6:
+#		print('comp', comp, 'user', user, "no one is win")
+#		break
+	
+# C O N T I N U E	i shnorhiv ete na gtav R tary ancnum a araj
+
+#name = 'Arsen'
+#for i in name:
+#	if i == 'r':
+#		continue
+#	print(i)
