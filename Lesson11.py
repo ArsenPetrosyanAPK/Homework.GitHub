d52b968fc4960e893394b2ec6341e9fbc241aae8
ArsenPetrosianAPK <arsen.apk@mail.ru>
2020-03-12 21:02:32 +0400

    lesson11

---
+++ Lesson11.py
@@ -0,0 +1,92 @@
+#fruits = ['bannana', 'apple', 'cherry']
+#print(fruits)
+
+# NEGATIVE INDEXING
+
+#numbers = [34, 56, -456, 7.56, -2.34]
+#print(numbers[-1])
+
+
+#############
+
+#clas = ['Artur', 'Armen', 'Arsen', 'Arno', 'Levon', 'Ani', 'Mariam', 'Nune','Nare', 'Artyom']
+
+
+
+#for i in clas:
+#	if i != clas:
+#		print(i, 'not in class')
+
+###########
+
+#fruits = ['Banana', 'apple', 'cherry']
+#print(fruits[1:3])
+#print(fruits[:3])
+#print(fruits[1:])
+#print(fruits[-3: -1])
+
+##### change item value ######
+
+
+#fruits[1] = 'kiwi'
+#print(fruits)
+
+####################
+# LEN listi erkarutyuny
+
+#print(len(fruits))
+
+################]
+
+#fruits.insert(1, 'Orange')
+#print(fruits)
+
+############
+
+#fruits.remove('apple')
+#print(fruits)
+
+################
+# vercnum e verji indexy
+#fruits.pop()
+##
+#print(fruits)
+#c = fruits.pop()
+#print(c)
+##################
+#del fruits[1]
+##
+#del fruits
+#print(fruits)
+#################
+#extend kpcnum e irar
+#numbers = [34, 56, -456, 7.56, -2.34]
+#fruits.extend(numbers)
+#print(fruits)
+############
+#amenamec arjeqy
+
+####numbers.sort()
+#print(numbers[-1])
+###############
+#numbers.reverse()
+###print(numbers)
+
+#####################
+
+#clas = ['Artur', 'Armen', 'Arsen', 'Arno', 'Levon', 'Ani', 'Mariam', 'Nune','Nare', 'Artyom']
+#new_class = []
+#for i in clas:
+#	if len(clas) >= 5:
+#		new_class = clas[:5]
+#print(new_class)
+###########################
+#numbers = [34, 56, -456, 7.56, -2.34]
+#numbers.sort()
+#print(numbers[-3: ])
+##
+#print(numbers[:3])
+
+
+#TNAYIN TPEL LISTI MEJI AMENAMEC TIVY ARANC .sort() I
+
